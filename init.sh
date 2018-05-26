91a8be1ac062f2e416aef33696f7a844421095dc
=anthony <=contact@anthonyhumphreys.co.uk>
2018-05-26 12:36:28 +0100

    add init script

---
+++ init.sh
@@ -0,0 +1,2 @@
+git clone https://tebayhumphreys.visualstudio.com/Platform/Platform%20Team/_git/applex-build-stack
+git clone https://tebayhumphreys.visualstudio.com/Platform/Platform%20Team/_git/thss-applex-build-frontend
